efa872be8861ff28b9f155e72d7fd5faa10fcce8
steppa <stephhousden@gmail.com>
2019-06-20 12:36:00 +0100

    Looking at snaphot render testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import renderer from 'react-test-renderer';
 import App from './App';
+import ArticleSearch from './components/ArticleSearch';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+
+it('renders correctly', () => {
+  const tree = renderer
+    .create(<ArticleSearch/>)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
 });
+
